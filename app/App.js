7525ffab77eefaf743abb4ee0808be17d01c9be1
zchen133 <56332780+zchen133@users.noreply.github.com>
2019-10-14 15:32:25 -0700

    Updated App.js

--- app/App.js
+++ app/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import * as firebase from "firebase";
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 
-  var firebaseConfig = {
+  var config = {
     apiKey: "AIzaSyC7m3ww22OXsKOiV11C4AG5wWCZyYHgsoo",
     authDomain: "woo-firebase.firebaseapp.com",
     databaseURL: "https://woo-firebase.firebaseio.com",
@@ -12,7 +13,7 @@ import { StyleSheet, Text, View, Button } from 'react-native';
     appId: "1:579750847600:web:ee340682ff4f9c9e2d80b5",
     measurementId: "G-ZKGHZERYG4"
   };
-  
+  firebase.initializeApp(config);
 
 
 
