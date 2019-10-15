002b5f4e567b402ff9565106e98a147bb68d7471
Vineet Ramareddi <vramared@ucsc.edu>
2019-10-14 19:28:28 -0700

    created src folder to hold pages

--- app/App.js
+++ app/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import * as firebase from "firebase";
 import { StyleSheet, Text, View, Button } from 'react-native';
+import Signup from './src/Signup';
 
 
-  var config = {
+var config = {
     apiKey: "AIzaSyC7m3ww22OXsKOiV11C4AG5wWCZyYHgsoo",
     authDomain: "woo-firebase.firebaseapp.com",
     databaseURL: "https://woo-firebase.firebaseio.com",
@@ -12,25 +13,23 @@ import { StyleSheet, Text, View, Button } from 'react-native';
     messagingSenderId: "579750847600",
     appId: "1:579750847600:web:ee340682ff4f9c9e2d80b5",
     measurementId: "G-ZKGHZERYG4"
-  };
-  firebase.initializeApp(config);
+};
 
+firebase.initializeApp(config);
 
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>test</Text>
-      <Button title = "test" />
-    </View>
-  );
+export default class Woo extends Component {
+    render() {
+        return (
+            <Signup />
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
