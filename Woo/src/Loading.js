0675c7bdf0da2b8b2cb59a7d4bc6e9fa420506ae
Vineet Ramareddi <vramared@ucsc.edu>
2019-10-20 22:31:51 -0700

    loading fix

--- Woo/src/Loading.js
+++ Woo/src/Loading.js
@@ -4,10 +4,25 @@ import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
 export default class Loading extends Component {
 
+    user = firebase.auth().currentUser;
+
     componentDidMount() {
+        if (this.user) {
+            // User is signed in.
+            console.log(this.user.getEmail());
+        } else {
+            // No this.user is signed in.
+            console.log('No user');
+            this.props.navigation.navigate('Login')
+        }
+        /*
         firebase.auth().onAuthStateChanged(user => {
+            if(user) {
+                console.log(user.getEmail());
+            }
             this.props.navigation.navigate(user ? 'Home' : 'Login')
         })
+        */
     }
 
     render() {
