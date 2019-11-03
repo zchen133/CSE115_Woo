9a8104768b42b0ca16e9e355d59a9224e208749a
Howard Ho Wai Ng <hhng@ucsc.edu>
2019-11-03 00:00:24 -0700

    Profile UI finished

---
+++ Woo/src/EditableTextComponent.js
@@ -0,0 +1,114 @@
+//https://github.com/ElinaSchaefer77/react-native-inline-edit by ElinaSchaefer77
+import React from 'react';
+import { View, Text, TextInput, TouchableOpacity, ActivityIndicator } from 'react-native';
+import PropTypes from 'prop-types';
+
+const initialState = {
+  text: '',
+  editing: false,
+};
+
+const styles = {
+  spinnerStyle: {
+    position: 'absolute',
+  },
+};
+
+class EditableText extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+  }
+
+  startEditing() {
+    if (this.props.isTextEditable) {
+      this.setState({
+        editing: true,
+        text: this.props.text,
+      });
+    }
+  }
+
+  stopEditing() {
+    this.props.sendText(this.state.text);
+    this.setState({
+      editing: false,
+    });
+  }
+
+  renderText() {
+    if (!this.state.editing && !this.props.loading) {
+      return (
+        <TouchableOpacity onPress={() => this.startEditing()}>
+          <Text style={this.props.textProps}>
+            {this.props.text}
+          </Text>
+        </TouchableOpacity>
+      );
+    }
+    return null;
+  }
+
+  renderTextInput() {
+    if (this.state.editing || this.props.loading) {
+      return (
+        <View>
+          <View>
+            <TextInput
+              autoFocus
+              opacity={this.state.editing ? 1 : 0.1}
+              {...this.props.textInputProps}
+              onChangeText={text => this.setState({ text })}
+              value={this.state.text}
+              returnKeyType={'done'}
+              onBlur={() => this.stopEditing()}
+            />
+          </View>
+          {this.renderActivtyIndicator()}
+        </View>
+      );
+    }
+    return null;
+  }
+
+  renderActivtyIndicator() {
+    if (this.props.loading) {
+      return (
+        <View
+          style={styles.spinnerStyle}
+        >
+          <ActivityIndicator size={'small'} />
+        </View>
+      );
+    }
+    return null;
+  }
+
+  render() {
+    return (
+      <View>
+        {this.renderText()}
+        {this.renderTextInput()}
+      </View>
+    );
+  }
+}
+
+EditableText.propTypes = {
+  isTextEditable: PropTypes.bool,
+  text: PropTypes.string.isRequired,
+  sendText: PropTypes.func.isRequired,
+  textProps: PropTypes.shape(),
+  textInputProps: PropTypes.shape(),
+  loading: PropTypes.bool,
+};
+
+EditableText.defaultProps = {
+  isTextEditable: true,
+  textProps: {},
+  textInputProps: {},
+  loading: false,
+};
+
+export default EditableText;
